d89c06baae3b1f687ad301036ed80cedd4bc4207
snehasingh-25 <sneha251104@gmail.com>
2025-09-30 01:33:31 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import gsap from "gsap"
+import Navbar from "./components/Navbar"
 import {ScrollTrigger,SplitText} from "gsap/all"
+import Hero from './components/Hero'
 gsap.registerPlugin(ScrollTrigger,SplitText)
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </main>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,109 @@
+import React, { useRef } from 'react'
+import gsap from "gsap"
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { useMediaQuery } from 'react-responsive'
+
+
+const Hero = () => {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', {
+            type: 'chars,words'
+        })
+        const paraSplit = new SplitText('.subtitle', {
+            type: 'lines'
+        })
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'))
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06
+        });
+
+        gsap.from(paraSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1
+        })
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+                // pin:true
+            }
+        }).to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
+
+        const start = isMobile ? 'top 50%' : 'center 60%';
+        const end = isMobile ? '120% top' : 'bottom top';
+
+        let videoTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: start,
+                end: end,
+                scrub: true,
+                pin: true
+            }
+        });
+
+        videoRef.current.addEventListener("loadedmetadata", () => {
+            videoTimeline.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            });
+        });
+
+
+    }, [])
+    return (
+        <>
+            <section id="hero" className=''>
+                <h1 className='title'>MOJITO</h1>
+                <img src="/images/hero-left-leaf.png" alt=""
+                    className='left-leaf' />
+                <img src="/images/hero-right-leaf.png" alt=""
+                    className='right-leaf' />
+
+                <div className='body'>
+                    <div className='content'>
+                        <div className='space-y-5 hidden md:block'>
+                            <p>Cool, Crisp, Classic</p>
+                            <p className='subtitle'>Sip the Spirit
+                                <br />of Summer
+                            </p>
+                        </div>
+                        <div className="view-cocktails">
+                            <p className="subtitle">
+                                Every cocktail on our menu is a blend of premium ingredients,
+                                creative flair, and timeless recipes — designed to delight your
+                                senses.
+                            </p>
+                            <a href="#cocktails">View cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className='video absolute inset-0'>
+                <video
+                    ref={videoRef}
+                    src="/videos/input.mp4" muted
+                    playsInline
+                    preload='auto'
+                ></video>
+            </div>
+        </>
+    )
+}
+
+export default Hero
